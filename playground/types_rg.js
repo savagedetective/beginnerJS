21fc8549f8bc4d71515d8d3d984da1606f04778b
Ritchie <detectives.salvajes@protonmail.com>
2020-01-28 18:21:51 -0500

    end of module 1

---
+++ playground/types_rg.js
@@ -0,0 +1,17 @@
+console.log('I work');
+
+/* eslint-disable */
+
+const name = 'ritch';
+
+const middle = "middle name";
+
+const last = `last name`;
+
+const hello = `Hello my name is ${name}. Interpolation is fun`;
+
+const person = {
+    first: "ritchie",
+    last: 'last name',
+    age : 22222
+};
