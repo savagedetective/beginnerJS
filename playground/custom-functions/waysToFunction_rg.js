70a8eea6db559fc4c00dd26792203bb900ca5da4
Ritchie <detectives.salvajes@protonmail.com>
2020-02-04 18:03:30 -0500

    playing with functions

---
+++ playground/custom-functions/waysToFunction_rg.js
@@ -0,0 +1,17 @@
+console.log('I work!');
+
+// function add(a, b = 3) {
+//   const total = a + b;
+//   return total;
+// }
+
+// const addition = (a, b = 3) => a + b;
+
+// console.log(addition(1, 2));
+
+// IIFE Immediately Invoked Function Expression
+// (function() {
+//   console.log('THIS IIFE HAS BEEN II.');
+// })();
+
+// METHODS are functions that live inside of an object
