ca6c86fc9aefd4badef1785d54b7b669efa3a03a
Ritchie <detectives.salvajes@protonmail.com>
2020-02-24 18:26:19 -0500

    video 21 done

---
+++ exercises/20 - The DOM/the-dom.js
@@ -0,0 +1,8 @@
+const p = document.querySelector('p');
+console.log(p);
+
+const divs = document.querySelectorAll('div');
+console.log(divs);
+
+const imgs = document.querySelectorAll('.item img');
+console.log(imgs);
