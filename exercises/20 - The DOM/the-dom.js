c9d2846ba28e76c3caea784a995fc983bfe76957
Ritchie <detectives.salvajes@protonmail.com>
2020-02-26 18:03:28 -0500

    video 23 done

--- exercises/20 - The DOM/the-dom.js
+++ exercises/20 - The DOM/the-dom.js
@@ -1,19 +1,38 @@
-const p = document.querySelector('p');
-console.log(p);
+// video 21 and 22
 
-const divs = document.querySelectorAll('div');
-console.log(divs);
+// const p = document.querySelector('p');
+// console.log(p);
 
-const imgs = document.querySelectorAll('.item img');
-console.log(imgs);
+// const divs = document.querySelectorAll('div');
+// console.log(divs);
 
-const heading = document.querySelector('h2');
-console.log(heading.textContent);
-console.log(heading.innerText);
+// const imgs = document.querySelectorAll('.item img');
+// console.log(imgs);
 
-// adding pizzas to html
-const pizzaList = document.querySelector('.pizza');
-console.log(pizzaList.textContent);
-// pizzaList.textContent = `${pizzaList.textContent} 🍕`;
-// the above can be slow
-pizzaList.insertAdjacentText('beforeend', '🍕');
+// const heading = document.querySelector('h2');
+// console.log(heading.textContent);
+// console.log(heading.innerText);
+
+// // adding pizzas to html
+// const pizzaList = document.querySelector('.pizza');
+// console.log(pizzaList.textContent);
+// // pizzaList.textContent = `${pizzaList.textContent} 🍕`;
+// // the above can be slow
+// pizzaList.insertAdjacentText('beforeend', '🍕');
+
+// video 23
+
+const pic = document.querySelector('.nice');
+pic.classList.add('open');
+pic.classList.remove('cool');
+console.log(pic.classList);
+
+// pic.addEventListener('click', function() {
+//   pic.classList.toggle('round');
+// });
+
+function toggleRound() {
+  pic.classList.toggle('round');
+}
+
+pic.addEventListener('click', toggleRound);
