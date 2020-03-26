302f03ddf73eecef9d3b1c51c0ecbf5807877c25
Ritchie <detectives.salvajes@protonmail.com>
2020-03-26 17:56:41 -0400

    video 30 done

--- exercises/29 - Events/events_rg.js
+++ exercises/29 - Events/events_rg.js
@@ -14,11 +14,40 @@ function handleClick() {
 
 const buyButtons = document.querySelectorAll('button.buy');
 
-function buyItem() {
-  console.log('buying item!');
+// video 29
+// function buyItem() {
+//   console.log('buying item!');
+// }
+
+// buyButtons.forEach(function(buyButton) {
+//   console.log('binding the buy button');
+//   buyButton.addEventListener('click', handleClick);
+// });
+// on to video 30
+
+function handleBuyButtonClick(event) {
+  console.log('you clicked a button');
+  const button = event.target;
+  console.log('You are buying it');
+  console.log(button);
+  // stop this event from bubbling up
+  // event.stopPropagation();
 }
 
 buyButtons.forEach(function(buyButton) {
-  console.log('binding the buy button');
-  buyButton.addEventListener('click', handleClick);
+  buyButton.addEventListener('click', handleBuyButtonClick);
+});
+
+window.addEventListener(
+  'click',
+  function(event) {
+    console.log('you clicked on the window', event.target);
+  },
+  { capture: true }
+);
+
+const photoEl = document.querySelector('.photo');
+photoEl.addEventListener('mousemove', function(event) {
+  console.log(event.currentTarget);
+  console.log(this);
 });
