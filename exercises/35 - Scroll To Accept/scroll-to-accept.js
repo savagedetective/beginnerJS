13a03665e79dbe82ca8aae4ad33a47f1c8a4f8f8
Ritchie <detectives.salvajes@protonmail.com>
2020-04-06 18:43:57 -0400

    video 35 done

--- exercises/35 - Scroll To Accept/scroll-to-accept.js
+++ exercises/35 - Scroll To Accept/scroll-to-accept.js
@@ -1,1 +1,28 @@
 console.log('IT WORKS!');
+
+function scrollToAccept() {
+  const terms = document.querySelector('.terms-and-conditions');
+  const button = document.querySelector('.accept');
+
+  if (!terms) {
+    return; // quits function if terms is not on the page
+  }
+
+  function obCallback(payload) {
+    console.log(payload[0].intersectionRatio);
+    if (payload[0].intersectionRatio === 1) {
+      button.disabled = false;
+    } else {
+      button.disabled = true;
+    }
+  }
+
+  const ob = new IntersectionObserver(obCallback, {
+    root: terms,
+    threshold: 1,
+  });
+
+  ob.observe(terms.lastElementChild);
+}
+
+scrollToAccept();
